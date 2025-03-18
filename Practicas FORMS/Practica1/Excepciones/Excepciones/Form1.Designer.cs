6371669b6ad338ae4dc814a6dd4ad04ee8b72ac1
Andy94m <m.andresmontes@gmail.com>
2025-03-18 19:39:19 -0300

    Manejo de excepciones 1

---
+++ Practicas FORMS/Practica1/Excepciones/Excepciones/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Excepciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BTN1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // BTN1
+            // 
+            this.BTN1.Location = new System.Drawing.Point(324, 213);
+            this.BTN1.Name = "BTN1";
+            this.BTN1.Size = new System.Drawing.Size(75, 23);
+            this.BTN1.TabIndex = 2;
+            this.BTN1.Text = "Calcular";
+            this.BTN1.UseVisualStyleBackColor = true;
+            this.BTN1.Click += new System.EventHandler(this.BTN1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTN1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BTN1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Practicas FORMS/Practica1/Excepciones/Excepciones/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excepciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN1_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+
+            try
+            {
+                a = int.Parse(textBox1.Text);
+                b = int.Parse(textBox2.Text);
+                r = a + b;
+                label1.Text = " = " + r;
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Por favor cargar solo numeros");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("No se puede dividir por cero");
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("Sobrecargó el int");
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Hubo un error al ejecutar...");
+            }
+
+
+        }
+    }
+}
