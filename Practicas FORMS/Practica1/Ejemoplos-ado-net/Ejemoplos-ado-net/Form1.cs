b31c38f31c102824026a920dbf00fe64d7415be6
Andy94m <m.andresmontes@gmail.com>
2025-03-19 20:24:39 -0300

    conexión a base de datos POKE

---
+++ Practicas FORMS/Practica1/Ejemoplos-ado-net/Ejemoplos-ado-net/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemoplos_ado_net
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvPokemons_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvPokemons.DataSource = negocio.listar();
+        }
+    }
+}
