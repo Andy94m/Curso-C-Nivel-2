17a9e0be4fa651f7f495292a0bd00a7a49c5afb7
Andy94m <m.andresmontes@gmail.com>
2025-03-19 21:10:59 -0300

    conexion db en Discos

---
+++ Practicas FORMS/Practica1/Discos/Discos/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discos
+{
+    internal class Album
+    {
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        public int Canciones { get; set; }
+    }
+}
---
+++ Practicas FORMS/Practica1/Discos/Discos/AlbumNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discos
+{
+    internal class AlbumNegocio
+    {
+        public List<Album> listar()
+        {
+            List<Album> lista = new List<Album>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            
+            try
+            {
+                conexion.ConnectionString = "server=localhost; database=DISCOS_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select ID, Titulo, CantidadCanciones from DISCOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Album aux = new Album();
+                    aux.Numero = (int)lector["Id"];
+                    aux.Nombre = (string)lector["Titulo"];
+                    aux.Canciones = (int)lector["CantidadCanciones"];
+
+                    lista.Add(aux);
+                }
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
