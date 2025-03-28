eb3b277b49dd94dfc84e3bb025dc7a12aa7a8dee
Andy94m <m.andresmontes@gmail.com>
2025-03-28 18:04:19 -0300

    centralizacion de datos en album

--- Practicas FORMS/Practica1/Discos/negocio/AccesoDatos.cs
+++ Practicas FORMS/Practica1/Discos/negocio/AccesoDatos.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 
 namespace negocio
 {
-    public class AccesoDatos
+    internal class AccesoDatos
     {
         private SqlConnection conexion;
         private SqlCommand comando;
         private SqlDataReader lector;
-        public SqlDataReader Lector
-        {
-            get { return lector; }
-        }
+        public SqlDataReader Lector { get; }
 
         public AccesoDatos()
         {
-            conexion = new SqlConnection("server=localhost; database=POKEDEX_DB; integrated security=true");
+            conexion = new SqlConnection("server=localhost; database=DISCOS_DB; integrated security=true");
             comando = new SqlCommand();
         }
 
@@ -45,9 +42,13 @@ namespace negocio
 
         public void cerrarConexion()
         {
-            if (lector != null)
+            if(lector != null)
                 lector.Close();
-            conexion.Close();
+            else
+            {
+                conexion.Close();
+            }
         }
+
     }
 }
---
+++ Practicas FORMS/Practica1/Discos/negocio/EstilosNegocio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    internal class EstilosNegocio
+    {
+        public List<Estilos> listar()
+        {
+            List<Estilos> lista = new List<Estilos>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion FROM ESTILOS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Estilos aux = new Estilos();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
+
